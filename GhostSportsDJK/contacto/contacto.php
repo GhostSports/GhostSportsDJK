df107d643a636fa1d971f59284aa6adf1ce92ad3
KevynAndres <kevynandresroa@gmail.com>
2025-08-13 16:48:26 -0500

    Add files via upload

---
+++ GhostSportsDJK/contacto/contacto.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="../index/estilosIndex.css">
+    <link rel="stylesheet" href="estiloscontacto.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/ef698667d1.js" crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+<body>
+    
+  <header>
+    <div class="contenedor-ingresar">
+        <a href="../ingresar/ingresar.php">
+            <button class="btn-ingresar">
+                <span>INICIAR SESION</span>
+            </button>
+        </a>
+        
+       
+    
+    </div>
+
+   
+    <div class="logo">
+        <a href="../index.php">
+            <img src="../img/imagen56-Photoroom.png" alt="Logo" width="150px" height="150px" class="atras">
+        </a>
+    </div>
+
+   
+    <div class="fondo">
+        <form>
+            <input type="text" placeholder="Busqueda en la Web">
+            <button><i class="fa-solid fa-magnifying-glass"></i></button>
+        </form>
+    </div>
+
+    
+        <div class="botones-principales">
+            <a href="../index.php">
+            <button><span>INICIO</span></button>
+        </a>
+        <a href="../acerca/acerca.php">
+            <button><span>ACERCA DE</span></button>
+        </a>
+        <a href="../portafolio/portafolio.php">
+            <button><span>PRODUCTOS</span></button>
+        </a>
+       
+    </div>
+</header>
+<section class="principalcontacto"> 
+ 
+    <section class="contacto">
+     
+      <img src="../img/1077114.png" alt="" width="90px" height="90px"  class="loguser">
+      <br>  
+    
+      <input type="text" placeholder="Nombre" class="cajatexto">
+      <input type="number" placeholder="Telefono" class="cajatexto">
+      <input type="email" placeholder="Email" class="cajatexto">
+      <input type="password" placeholder="Contraseña" class="cajatexto">
+      <a href="../ingresar/ingresar.php"><input type="submit" value="Registro" class="boton"></a>
+      
+  
+  
+    </section>
+
+</section>
+<footer>
+    <div>
+        <a href="https://www.facebook.com">
+            <img src="../img/facebookn.png" alt="Facebook" width="50px" height="50px">
+        </a>
+        <a href="https://x.com/?lang=es">
+            <img src="../img/xn.png" alt="Twitter/X" width="50px" height="50px">
+        </a>
+        <a href="https://www.instagram.com/">
+            <img src="../img/instan.png" alt="Instagram" width="50px" height="50px">
+        </a>
+    </div>
+
+    <div>
+        <p class="textofooter">https://ghostsports.github.io/GhostSportsDJK</p>
+    </div>
+</footer>
+
+</body>
+</html>
+</body>
+</html>
+
+
