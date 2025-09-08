c48dd9c88e0bfe28411dee3fa333d974c82cdf4a
Diegox26 <diegopamplona599@gmail.com>
2025-09-08 09:20:33 -0500

    Add files via upload

---
+++ GhostSportsDJK/GhostSportsDJK/ingresar/ingresar.php
@@ -0,0 +1,61 @@
+<?php
+ 
+if(isset($_POST['ingresar'])){
+
+$correo = $_POST['correo'];
+$contraseña = $_POST['contraseña'];
+
+$conn = mysqli_connect("localhost","root","","ghostsportsdjk");
+
+$sql = "select * from empleado where correo = '$correo' and contrasena = '$contraseña' ";
+
+$consulta = mysqli_query($conn,$sql);
+
+$registro = mysqli_num_rows($consulta);
+
+if ($registro>0)
+header("location: ../index2/index2.php");  
+
+}
+
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="../index/estilosIndex.css">
+    <link rel="stylesheet" href="estilosingresar.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+</nav>
+
+<section class="principalingresar"> 
+  <form action="ingresar.php" method="post">
+  <section class="ingresar">
+    <div>
+    <a href="../index.php"><img src="../img/atras.png" alt="" width="80px" height="80px" class="atras"></a>
+    <a href="../contacto/contacto.php" class="textoboton5">REGISTRESE</a>
+    </div>
+    <img src="../img/1077114.png" alt="" width="90px" height="90px"  class="loguser">
+    <br>
+    <input type="text" placeholder="correo" name=correo class="cajatexto">
+    <input type="password" placeholder="Contraseña" name=contraseña class="cajatexto">
+    <a href="../index.php"><input type="submit" value="INGRESAR" name="ingresar" class="boton"></a>
+ 
+    <input type="submit" value="¿Haz olvidado tu contraseña?" class="boton2">
+  </form>
+  </section>
+
+</section>
+</footer>
+</body>
+</html>
